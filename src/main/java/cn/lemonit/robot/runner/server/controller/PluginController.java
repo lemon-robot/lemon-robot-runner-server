79a4bcd5bad09e9e3a08e8ed3b926b98302e1981
LemonITCN <liuri@lemonit.cn>
2018-08-11 17:37:28 +0800

    准备开始编写插件控制器

--- src/main/java/cn/lemonit/robot/runner/server/controller/PluginController.java
+++ src/main/java/cn/lemonit/robot/runner/server/controller/PluginController.java
@@ -1,9 +1,22 @@
 package cn.lemonit.robot.runner.server.controller;
 
+import cn.lemonit.robot.runner.common.beans.general.Response;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  * 插件请求的Controller
  *
  * @author liuri
  */
+@RequestMapping("/plugin")
 public class PluginController {
+
+    @PostMapping("/upload")
+    public Response uploadPlugin(@RequestParam MultipartFile pluginFile) {
+
+    }
+
 }
